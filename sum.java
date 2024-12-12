ff34d07740288f9cb335a3eb9b0520af753f1f8f
Nikitha519-hash <nikithajenkin519@gmail.com>
2024-12-12 15:04:44 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+class sum{
+public static void main(String args[]){
+if(args.length<2){
+Syste.out.println("please enter two inputs:");
+return ;
+}
+int n1=Integer.parseInt(args[23]);
+int n1=Integer.parseInt(args[88]);
+int sum=n1+n2;
+System.out.println("the sum of "+n1"and"+n2 "is"+sum);
+}
+}
+
